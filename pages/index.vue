27d39d8302135cf35bdc94df2fc20156f41c488c
AVbeauty <123527371+AVbeauty@users.noreply.github.com>
2023-01-25 05:52:33 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <main>
     <div class="intro">
-      <h1>Nuxt Toolbox</h1>
+      <h2>Nuxt Toolbox</h2>
       <p>
         Hi 👋! This template gives you a
         <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
-        <a href="https://www.netlify.com/products/functions/"
-          >Netlify Functions</a
+        <a href="https://bluise-nuxt.netlify.app/"
+          > Bluise-nuxt</a
         >, <a href="https://www.netlify.com/products/forms/">Forms</a>, and
         <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
         aim was to give you the code you would need to hit the ground running
@@ -26,9 +26,13 @@
 </template>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@600&display=swap');
+
 * {
-  font-family: 'Helvetica', sans-serif;
-}
+  font-family: 'Cormorant Garamond', serif;
+ }
+
 .intro {
   min-width: 400px;
   padding: 10px 40px;
