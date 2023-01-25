fe90d7682972a942f00e9b53b7fb0e890f684887
AVbeauty <123527371+AVbeauty@users.noreply.github.com>
2023-01-25 05:48:26 +0200

    Update success.vue

--- pages/success.vue
+++ pages/success.vue
@@ -4,3 +4,17 @@
     <NuxtLink to="/">Home</NuxtLink>
   </div>
 </template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@600&display=swap');
+
+
+* {
+  font-family: 'Cormorant Garamond', serif;
+}
+.intro {
+  min-width: 400px;
+  padding: 10px 40px;
+  width: 60%;
+}
+</style>
